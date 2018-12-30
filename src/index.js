7819a9fbbbe358ac76820e0115746ef16b1bed48
yano3 <yano3@alhino.jp>
2018-12-30 14:41:01 +0900

    Add src dir.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import '@babel/polyfill'
+import 'whatwg-fetch'
+import 'url-polyfill'
+import 'intersection-observer'
+import 'animate.css'
+import 'bootstrap'
+import 'font-awesome/css/font-awesome.min.css'
+import smoothscroll from 'smoothscroll-polyfill'
+
+// Execute polyfills.
+smoothscroll.polyfill()
